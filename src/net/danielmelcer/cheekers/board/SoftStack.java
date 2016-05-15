cf497533963135b978f8b59c2c761eb2f699dc42
dmelcer9 <dmelcer9@gmail.com>
2016-05-15 18:21:39 -0400

    Worked of SoftStack

--- src/net/danielmelcer/cheekers/board/SoftStack.java
+++ src/net/danielmelcer/cheekers/board/SoftStack.java
@@ -1,5 +1,8 @@
 package net.danielmelcer.cheekers.board;
-
+import java.lang.ref.SoftReference;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * This class manages a stack of a given type. References sufficiently below the top of the stack are placed into SoftReferences.
@@ -9,22 +12,53 @@ package net.danielmelcer.cheekers.board;
  */
 public class SoftStack<T> {
 	
+	private int strongLimit;
+	
+	private Stack<SoftReference<T>> softr;
+	private LinkedList<T> strongr;
+	
 	/**
 	 * Constructs a SoftStack with a given limit. This will define how many strong references the SoftStack will store.
 	 * Any additional items will push an item into a SoftReference
 	 * @param strongReferenceLimit The maximum number of items before Soft references are used
 	 */
 	public SoftStack(int strongReferenceLimit){
-		//TODO: Implement
+		this.strongLimit = strongReferenceLimit;
+		
+		softr = new Stack<>();
+		strongr = new LinkedList<>();
+		
+	}
+	
+	private void shiftElements(){
+		
+		boolean softEmpty = softr.isEmpty() || (softr.peek().get() == null);
+		if(softEmpty) softr.clear();
+		
+		if(strongr.size() <= strongLimit){
+			if(softEmpty){
+				return;
+			} else{
+				while(!softEmpty && strongr.size() < strongLimit){
+					strongr.addLast(softr.pop().get());
+					softEmpty = softr.isEmpty() || (softr.peek().get() == null);
+				}
+			}
+		} else{
+			while(strongr.size() > strongLimit){
+				softr.add(new SoftReference<>(strongr.removeLast()));
+			}
+		}
+		
 	}
 	
 	/**
 	 * Returns true if the next item is available
 	 * @return A boolean corresponding to if an item is available
 	 */
-	public boolean available() throws IllegalStateException{
-		//TODO: Implement
-		return false;
+	public boolean available(){
+		shiftElements();
+		return !strongr.isEmpty();
 	}
 	
 	/**
@@ -32,7 +66,8 @@ public class SoftStack<T> {
 	 * @param element The item to add to the stack
 	 */
 	public void push(T element){
-		//TODO: Implement
+		strongr.addFirst(element);
+		shiftElements();
 	}
 	
 	/**
@@ -40,7 +75,9 @@ public class SoftStack<T> {
 	 * @return The top item of the stack or null if none exists
 	 */
 	public T pop(){
-		//TODO: Implement
-		return null;
+		if(!available()) return null;
+		T ret = strongr.removeFirst();
+		shiftElements();
+		return ret;
 	}
 }
--- test/net/danielmelcer/cheekers/board/TestSoftStack.java
+++ test/net/danielmelcer/cheekers/board/TestSoftStack.java
@@ -2,38 +2,97 @@ package net.danielmelcer.cheekers.board;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSoftStack {
 
+	SoftStack<Integer> ss;
+	
+	@Before
+	public void setUp(){
+		ss = new SoftStack<>(3);
+	}
+	
 	@Test
 	public void testNoneAvailableAtStart() {
-		fail("Not yet implemented");
+		assertFalse(ss.available());
 	}
 	
 	@Test
 	public void testReturnsNullWhenNoneAvailable() {
-		fail("Not yet implemented");
+		assertNull(ss.pop());		
 	}
 	
 	@Test
 	public void testAvailableAfterPush() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testNotAvailableWhenEmpty() {
-		fail("Not yet implemented");
+		ss.push(5);
+		assertTrue(ss.available());
+		
+		assertTrue(ss.pop() == 5);
+		
+		assertFalse(ss.available());
+		
+		assertNull(ss.pop());
 	}
 	
 	@Test
 	public void testPopOutOfSoftReference() {
-		fail("Not yet implemented");
+		ss.push(1);
+		ss.push(2);
+		ss.push(3);
+		ss.push(4);
+		ss.push(5);
+		
+		assertTrue(ss.pop() == 5);
+		assertTrue(ss.pop() == 4);
+		assertTrue(ss.pop() == 3);
+		
+		assertTrue(ss.available());
+		
+		assertTrue(ss.pop() == 2);
+		assertTrue(ss.pop() == 1);
+		
+		assertFalse(ss.available());
+		
+		assertNull(ss.pop());
 	}
 	
 	@Test
 	public void testOutOfMemory() {
-		fail("Not yet implemented");
+		ss.push(1);
+		ss.push(2);
+		ss.push(3);
+		ss.push(4);
+		ss.push(5);
+		
+		OOM();
+		
+		System.out.println(ss.pop());
+		System.out.println(ss.pop());
+		System.out.println(ss.pop());
+		System.out.println(ss.pop());
+		System.out.println(ss.pop());
+		
+		//assertTrue(ss.pop() == 5);
+		//assertTrue(ss.pop() == 4);
+		//assertTrue(ss.pop() == 3);
+		
+		//assertFalse(ss.available());
+		//assertNull(ss.pop());
+	}
+	
+	public void OOM(){
+		try {
+	        final LinkedList<Object[]> allocations = new LinkedList<Object[]>();
+	        int size;
+	        while( (size = Math.min(Math.abs((int)Runtime.getRuntime().freeMemory()),Integer.MAX_VALUE))>0 )
+	            allocations.add( new Object[size] );
+	    } catch( OutOfMemoryError e ) {
+	        // great!
+	    }
 	}
 
 }
