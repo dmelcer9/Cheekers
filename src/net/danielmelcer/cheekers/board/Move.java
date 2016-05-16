4ed02d6790cb4399b11b81b19a02ace8efea84f0
dmelcer9 <dmelcer9@gmail.com>
2016-05-15 20:38:59 -0400

    Implemented Move and wrote test cases

--- src/net/danielmelcer/cheekers/board/Move.java
+++ src/net/danielmelcer/cheekers/board/Move.java
@@ -1,5 +1,7 @@
 package net.danielmelcer.cheekers.board;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * A move object represents a single turn of the game. It is an immutable object and can hold a sequence of positions
@@ -10,22 +12,47 @@ import java.util.Iterator;
  */
 public class Move implements Iterable<Coordinate>{
 	
+	
+	private Coordinate[] coordinates;
 	/**
 	 * Constructs a Move with the selected coordinates. 
 	 * @param coordinates A list of coordinates involved in the move, starting with the initial position.
 	 */
 	public Move(Coordinate... coordinates){
-		//TODO: Implement
+		if(coordinates.length < 2) throw new IllegalArgumentException("Moves must have a length of greater than two");
 		
+		this.coordinates = coordinates;	
 	}
 	
 	/**
 	 * Returns an iterator which iterates through the coordinates contained within the move
 	 * @return An iterator containing a list of coordinates
 	 */
-	public Iterator<Coordinate> iterator(){
-		//TODO: Implement
-		return null;
+	public Iterator<Coordinate> iterator(){		
+		return new MoveIterator(coordinates);
+	}
+	
+	private static class MoveIterator implements Iterator<Coordinate>{
+
+		private Coordinate[] c;
+		
+		int next = 0;
+		
+		public MoveIterator(Coordinate[] c){
+			this.c = c;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return (next < c.length);
+		}
+
+		@Override
+		public Coordinate next() {
+			if(!hasNext()) throw new NoSuchElementException("Reached end of iterator");
+			return c[next++];
+		}
+		
 	}
 	
 	/**
@@ -33,8 +60,7 @@ public class Move implements Iterable<Coordinate>{
 	 * @return The initial coordinate represented by the move
 	 */
 	public Coordinate getStartingPosition(){
-		//TODO: Implement
-		return null;
+		return coordinates[0];
 	}
 	
 	/**
@@ -42,8 +68,7 @@ public class Move implements Iterable<Coordinate>{
 	 * @return The coordinates in the move
 	 */
 	public Coordinate[] getMoves(){
-		//TODO: Implement
-		return null;
+		return Arrays.copyOf(coordinates, coordinates.length);
 	}
 	
 }
--- test/net/danielmelcer/cheekers/board/TestMove.java
+++ test/net/danielmelcer/cheekers/board/TestMove.java
@@ -2,38 +2,102 @@ package net.danielmelcer.cheekers.board;
 
 import static org.junit.Assert.*;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import javax.naming.OperationNotSupportedException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 public class TestMove {
-
-	@Before
-	public void setUp() throws Exception {
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testNoMove(){
+		Move m = new Move();
 	}
-
-	@After
-	public void tearDown() throws Exception {
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testOneMove(){
+		Move m = new Move(new Coordinate(1,1));
 	}
-
+	
 	@Test
-	public void testMove() {
-		fail("Not yet implemented");
+	public void testTwoMoves(){
+		Move m = new Move(new Coordinate(1,1), new Coordinate(1,1));
 	}
 
 	@Test
 	public void testIterator() {
-		fail("Not yet implemented");
+		Coordinate c1 = new Coordinate(1,1);
+		Coordinate c2 = new Coordinate(2,2);
+		Coordinate c3 = new Coordinate(3,3);
+		
+		Move m = new Move(c1,c2,c3);
+		
+		Iterator<Coordinate> i = m.iterator();
+		
+		assertTrue(i.hasNext());
+		assertThat(c1, is(i.next()));
+		
+		assertTrue(i.hasNext());
+		assertThat(c2, is(i.next()));
+		
+		assertTrue(i.hasNext());
+		assertThat(c3, is(i.next()));
+		
+		assertFalse(i.hasNext());
+	}
+	
+	@Test(expected=UnsupportedOperationException.class)
+	public void testRemoveIterator(){
+		Move m = new Move(new Coordinate(1,1), new Coordinate(2,2));
+		
+		Iterator<Coordinate> i = m.iterator();
+		
+		i.next();
+		i.remove();
+	}
+	
+	@Test(expected=NoSuchElementException.class)
+	public void testEndOfIterator(){
+		Coordinate c1 = new Coordinate(1,1);
+		Coordinate c2 = new Coordinate(2,2);
+		Coordinate c3 = new Coordinate(3,3);
+		
+		Move m = new Move(c1,c2,c3);
+		Iterator<Coordinate> i = m.iterator();
+		
+		i.next();
+		i.next();
+		i.next();
+		i.next();
 	}
 
 	@Test
 	public void testGetStartingPosition() {
-		fail("Not yet implemented");
+		Coordinate c1 = new Coordinate(1,1);
+		Coordinate c2 = new Coordinate(2,2);
+		Coordinate c3 = new Coordinate(3,3);
+		
+		Move m = new Move(c1,c2,c3);
+		
+		assertThat(m.getStartingPosition(), is(c1));
 	}
 
 	@Test
 	public void testGetMoves() {
-		fail("Not yet implemented");
+		Coordinate c1 = new Coordinate(1,1);
+		Coordinate c2 = new Coordinate(2,2);
+		Coordinate c3 = new Coordinate(3,3);
+		
+		Move m = new Move(c1,c2,c3);
+		
+		Coordinate[] arr = {c1,c2,c3};
+		
+		assertArrayEquals(arr, m.getMoves());
 	}
 
 }
