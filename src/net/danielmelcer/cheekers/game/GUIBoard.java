745617e41073fb2b5b731647e25b21b4501b29c8
dmelcer9 <dmelcer9@gmail.com>
2016-05-25 07:54:50 -0400

    Worked on Board class

--- src/net/danielmelcer/cheekers/game/GUIBoard.java
+++ src/net/danielmelcer/cheekers/game/GUIBoard.java
@@ -1,15 +1,27 @@
 package net.danielmelcer.cheekers.game;
 
 import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.*;
+import net.danielmelcer.cheekers.board.*;
 
 /**
  * This class creates and displays the GUI for a checkers board. It will also take input and pass it to a MoveListener.
  * @author Daniel Melcer
  * @see MoveListener
  */
-public class GUIBoard {
+public class GUIBoard extends JFrame {
 	
 	private MoveListener ml;
+	private JPanel mainPanel;
+	private BoardDisplay bd;
+	
+	public GUIBoard(Board b){
+		super("Cheekers!");
+		mainPanel = new JPanel();
+		bd = new BoardDisplay(b);
+	}
 	
 	/**
 	 * This will set a MoveListener to accept a move that is performed. Only one MoveListener can be set at a time.
@@ -25,5 +37,23 @@ public class GUIBoard {
 	 */
 	public void setColor(Color c){
 		//TODO
+	}
+	
+	private class BoardDisplay extends JComponent{
+		
+		private volatile Board currentBoard;
+		
+		BoardDisplay(Board b){
+			this.currentBoard = b;
+		}
+		
+		public void setBoard(Board b){
+			currentBoard = b;
+			this.invalidate();
+		}
+		
+		@Override public void paint(Graphics g){
+			
+		}
 	}
 }
